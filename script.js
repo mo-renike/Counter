c6d190af79a6f23ca78f044134726ccef32c42b5
Morenike Oyewole <herroyalpianist@gmail.com>
2021-07-13 16:02:24 +0100

    init commit

---
+++ script.js
@@ -0,0 +1,25 @@
+const result = document.getElementById("display");
+const plus = document.getElementById("add");
+const minus = document.getElementById("lower");
+
+let int = 0;
+
+plus.addEventListener("click", () => {
+  int++;
+
+  if (int >= 50) {
+      int = 50
+  }
+
+  result.innerText = int
+});
+minus.addEventListener("click", () => {
+  int--;
+
+  if (int <= 0) {
+      int = 0
+  }
+
+  result.innerText = int
+});
+
